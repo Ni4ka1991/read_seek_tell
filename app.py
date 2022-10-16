cd4863a41c52bb7cd4a058abdcad7da93a097d66
Ni4ka1991 <astahova_nica@mail.ru>
2022-10-17 00:14:15 +0300

    Pavel's solution

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
 #! /usr/bin/env python3
-
 from os import system
 
-f = open('text.txt' , 'r' )
+f = open('text.txt' , 'br' )
 
-system("clear")
-end = ""
-i = 0
-bytes_count = 0
-while i != end:
-    i = f.read(1)
-    b = i.encode('utf-16')
-    int_val = int.from_bytes( b, "big" )
-    bytes_count += int_val
-    print( f"char{type(i)} >>> {i}  :   bytes in hex{type(b)} >>> {b} : int value >>> {int_val}" )
-#print( f"curent bytes count >>> {bytes_count/1024/1024} Mb" )
+# Pavel's solution >>>
 
-f.close()
+def read_by_10( file_obj ):
+    i = file_obj.read(10)
+    while len(i) > 0:
+        print(i)
+        i = file_obj.read(10)
+
+read_by_10( f )
+
--- test.py
+++ test.py
@@ -1,11 +1,18 @@
 #! /usr/bin/env python3
-
 from os import system
 
 f = open('text.txt' , 'br' )
 
 system("clear")
-i = f.read(40)
-print( f"i(40) >>> {i}" )
-f.close()
+
+mid = round( len( f.read())/2 )
+
+def read_by_10( file_obj ):
+    i = f.seek( mid )
+    i = file_obj.read(10)
+    while len(i) > 0:
+        print(i)
+        i = file_obj.read(10)
+
+read_by_10( f )
 
